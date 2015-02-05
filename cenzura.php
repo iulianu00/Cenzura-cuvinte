ca51ff12ec04235b0596b701d1ae462a9c881e41
iulianu00 <iuliandoran@gmail.com>
2015-02-05 18:13:00 +0200

    Update cenzura.php

--- cenzura.php
+++ cenzura.php
@@ -1,13 +1,16 @@
 <?php
 $cuvinte_vulgare = array('fut','muie','pula','coaie','sugi','cur','pizda','fuck','suck','dracu','cur','pola','sperma','Fut','Muie','Pula','Coaie','Sugi','Cur','Pizda','Fuck','Suck','Dracu','Cur','Pola','Sperma');
 $cenzura = '***';
-
-$text= strtolower($_GET['text_introdus']);
-
+$text= strtolower($_POST['text_introdus']);
 $new_text=str_replace($cuvinte_vulgare,$cenzura,$text);
 
 
+if(isset($_POST['text_introdus']) &&! empty ($_POST['text_introdus']))
+	
+{
+
 echo 'Textul cenzurat este:<br>';
 echo ucfirst($new_text);
- 
+
+}
  
