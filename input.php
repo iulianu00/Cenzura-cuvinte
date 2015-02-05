4e45e97a07f49922331c6a8756f5486687c71efc
iulianu00 <iuliandoran@gmail.com>
2015-02-05 18:13:30 +0200

    Update input.php

--- input.php
+++ input.php
@@ -2,7 +2,7 @@
 
 echo ' 
 
-<form action="index.php" >
+<form action="index.php" method="POST" >
 <textarea name="text_introdus" rows="6" cols="40"></textarea></br><br>
 <input type="submit" value="Submit"><br><br>
 	   
