41b6fec25b56cd00b3b08f119f492502d36b5fb3
iulianu00 <iuliandoran@gmail.com>
2015-02-05 17:40:11 +0200

    Create input.php

---
+++ input.php
@@ -0,0 +1,13 @@
+<?php
+
+echo ' 
+
+<form action="index.php">
+Introdu te rog textul tau:<br>
+<input type="text" name="text_introdus" size="60" height="4" >
+<br>
+<input type="submit" value="Submit">
+</form>
+	   
+	   ';
+	   
